9536aa2bcebe4a0f7f7414f3122ccdd2d1457c93
Varun Maitreya Eranki <varun.maitreya@gmail.com>
2019-11-05 09:01:16 +0100

    initialised project files

---
+++ road_runner/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from __future__ import absolute_import
+
+
+def main():
+    import roadrunner
+
+if __name__ == '__main__':
+    main()
---
+++ road_runner/roadrunner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+#import
+
+class roadrunner:
+    def __init__(self):
+        pass
+
+    #def
